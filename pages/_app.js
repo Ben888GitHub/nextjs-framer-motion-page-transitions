13edc81a22a853118748caf66998bfdc5baa62e1
Benedict <benedictryan80@gmail.com>
2023-05-24 22:40:20 +0700

    set mode to wait

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ import { AnimatePresence } from "framer-motion";
 function MyApp({ Component, pageProps, router }) {
   return (
     <AnimatePresence
-      exitBeforeEnter
-      // mode="wait"
+      // exitBeforeEnter
+      mode="wait"
       // initial={false}
       // onExitComplete={() => window.scrollTo(0, 0)}
     >
